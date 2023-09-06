1f52729cef345c50343d5074c742f0f2ae3a589b
priyanshunegi04 <priyanshunegi966@gmail.com>
2023-09-07 01:34:54 +0530

    first commit StudyNotion

---
+++ src/components/core/Catalog/Course_Slider.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/free-mode"
+import "swiper/css/pagination"
+// import "../../.."
+// Import required modules
+import { Autoplay, FreeMode, Pagination } from "swiper"
+
+// import { getAllCourses } from "../../services/operations/courseDetailsAPI"
+import Course_Card from "./Course_Card"
+
+function Course_Slider({ Courses }) {
+  return (
+    <>
+      {Courses?.length ? (
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={25}
+          loop={true}
+          freeMode={true}
+          modules={[FreeMode, Pagination , Autoplay]}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+          className="max-h-[30rem]"
+        >
+          {Courses?.map((course, i) => (
+            <SwiperSlide key={i}>
+              <Course_Card course={course} Height={"h-[250px]"} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <p className="text-xl text-richblack-5">No Course Found</p>
+      )}
+    </>
+  )
+}
+
+export default Course_Slider
